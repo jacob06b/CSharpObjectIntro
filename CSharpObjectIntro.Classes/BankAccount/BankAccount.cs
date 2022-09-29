ba9e182d16bcfc2a5f69d3a265ed5362553a4a75
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2022-09-29 15:43:50 +0100

    Add project files.

---
+++ CSharpObjectIntro.Classes/BankAccount/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpObjectIntro.Classes.BankAccount
+{
+    public class BankAccount
+    {
+        // As you complete each task make sure you test your code carefully
+        // Choose some combination of manual testing, Debug.Assert and unit tests
+
+        // Task One        
+        // The bank account should have a balance property        
+        // It should have a constructor that sets the initial balance (default zero) and the opening date (default today)
+        // It should have methods to deposit and withdraw/make payments from the account. 
+
+        // Task Two
+        // Give the option to set an overdraft limit
+        // Do not allow a withdrawal/payment if the overdraft limit is exceeded. You could return false or throw an exception.
+
+        // Task Three
+        // Create a new class called AccountTransaction.
+        // It could contain properties such as
+        // date, amount, category, counterparty, transactionType, description (optional)
+        // e.g 26/09/2022 16:45, -300, Groceries, Waitrose, Card, Weekly shop
+        //     27/09/2022 17:40, 200, Gift, Bob Smith, Cheque, Birthday present
+        // You might wish to use enums for category and transactionType
+        // Amend your bank account to contain a list of transactions
+        // The methods for  deposit and withdraw/make payments should be amended to add transactions
+
+        // Task Four
+        // Now add some new methods to your account
+        // - See what the balance was at a previous date
+        // - See how much money was spent in a given time period
+        // - See how much money was spent in different categories
+
+        // Extension
+        // Work out how much interest is payable on your account
+        // For the moment make up the interest rates, though later we could look at loading them from a website
+        // The interest should be added as transactions to your account
+
+
+
+
+
+    }
+}
---
+++ CSharpObjectIntro/OOPExercises.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpObjectIntro.Classes.Diary;
+using CSharpObjectIntro.Classes.BankAccount;
+
+namespace CSharpObjectIntro
+{
+    internal class OOPExercises
+    {
+        internal static void Run()
+        {
+            UseDiary();
+        }
+
+        internal static void UseDiary()
+        {
+            // Create a new diary 
+            // Diary diary = new Diary("Bob Smith"); 
+
+            // Add some events to your diary
+
+            // Now check how many events you have on a particular day
+
+            // Implement a new method in the Diary class to return the number of morning events on a particular day
+            // Call this method
+        }
+
+        internal static void UseBankAccount()
+        {
+            // Implement your bank account class following the instructions in the BankAccount class
+
+            // Write calling code from here
+        }
+    }
+}
+
